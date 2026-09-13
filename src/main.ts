1376d1faf20fc32c56a815c20805bf4d1fba6dd8
TbotaPhantA <botakiller@mail.ru>
2026-09-13 19:25:11 +0000

    several useful utilities were added

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { ensureTruthy } from './infra/shared/utils/ensureTruthy.js';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(ensureTruthy(process.env.PORT));
 }
 await bootstrap();
